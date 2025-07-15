93dd0f2ead0c4f568ce23a331921c68555873dc1
RahifM <rahifmanjatha372@gmail.com>
2025-07-15 18:26:43 +0530

    wip: add the encode part

--- f-new.sh
+++ f-new.sh
@@ -1,13 +1,19 @@
-mkdir tm && cd tm && wget https://raw.githubusercontent.com/RahifM/scripts/refs/heads/newffmpeg/tm2.sh && chmod +x tm2.sh && ./tm2.sh && transmission-remote --start-paused -a "https://github.com/zmzu/dump/releases/download/1.0/Bleach.Box.1-6.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.torrent" && transmission-remote -t 1 -G all && transmission-remote -t 1 -g1 && transmission-remote -t 1 -f | grep Yes && transmission-remote -t 1 -s
+mkdir tm && cd tm && wget https://raw.githubusercontent.com/RahifM/scripts/refs/heads/newffmpeg/tm2.sh && chmod +x tm2.sh && ./tm2.sh && transmission-remote --start-paused -a "https://github.com/zmzu/dump/releases/download/1.0/Bleach.Box.1-6.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.torrent" && transmission-remote -t 1 -G all && transmission-remote -t 1 -g2 && transmission-remote -t 1 -f | grep Yes && transmission-remote -t 1 -s
+
+fmpg() {
+	cd Bl* && cd Bl* && wget https://raw.githubusercontent.com/RahifM/scripts/refs/heads/newffmpeg/f.sh && chmod +x f.sh && ./f.sh
+}
 
 while true ; do
 	if [ "$(transmission-remote -t 1 -i | grep State)" = "  State: Idle" ] ; then
-		echo "ilding"
+		echo "Idling, starting encode"
 		transmission-remote -t 1 -S
+		ls && pwd
+		fmpg
 		break
 	fi
 	echo $(transmission-remote -t 1 -i | grep State)
 	echo $(transmission-remote -t 1 -i | grep Percent)
-	echo "check back after 30s"
-  sleep 30
+	echo "check back after 15s"
+  sleep 15
 done
