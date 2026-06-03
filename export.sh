eb93dd312e00d9cf02c0c0e62ea05921569a842b
realraf37 <realraf37@gmail.com>
2026-06-03 14:20:37 +0530

    e121

--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
-export t=119
-export i=Bleach.E120.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.mkv
-export o=Bleach.E120.mkv
+export t=120
+export i=Bleach.E121.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.mkv
+export o=Bleach.E121.mkv
 export tl=https://github.com/zmzu/dump/releases/download/1.0/Bleach.Box.1-6.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.torrent
